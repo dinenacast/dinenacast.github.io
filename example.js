15b3262115346b4f57d16765fd8874bf50811298
dinenacast <dinenacast@gmail.com>
2020-08-05 14:54:16 -0300

    :sunny: New feedback system

--- example.js
+++ example.js
@@ -5,3 +5,26 @@ function alertTw() {
 function alertYt() {
     window.open('https://www.youtube.com/channel/UCPkZrRC-CiMrlo-JaciefBg')
 }
+
+function enviarFeedback() {
+    var nome = document.getElementById('nome').value
+    var feedbackText = document.getElementById('feedbackText').value
+
+    var thankFeedback = document.getElementById('thankFeedback')
+    var thankFeedbackName = document.getElementById('thankFeedbackName')
+
+    var feedbackBoxTitleFeedback = document.getElementById('feedbackBoxTitleFeedback')
+    var feedbackBoxTitleFeedbackH1 = document.getElementById('feedbackBoxTitleFeedbackH1')
+
+    var enviarFeedbackBtn = document.getElementById('enviarFeedbackBtn')
+
+    thankFeedback.hidden = false;
+    feedbackBoxTitleFeedback.hidden = true;
+    feedbackBoxTitleFeedbackH1.hidden = true;
+    thankFeedbackName.hidden = false;
+    enviarFeedbackBtn.hidden = true;
+
+    thankFeedbackName.innerHTML = ` ${nome}`
+
+    console.log(`${nome} disse: ${feedbackText}`)
+}
