0ec6913722aeb64c194e46d6a29a17a038d8b6fb
Igor Duca <igorducca@gmail.com>
2020-08-06 14:23:53 -0300

    :sunny: New css features and twitch alert

--- script.js
+++ script.js
@@ -6,42 +6,10 @@ function alertYt() {
     window.open('https://www.youtube.com/channel/UCPkZrRC-CiMrlo-JaciefBg')
 }
 
-function enviarFeedback() {
-    var nome = document.getElementById('nome').value
-    var feedbackText = document.getElementById('feedbackText').value
+function twitchAlert() {
+    event.preventDefault()
 
-    var thankFeedback = document.getElementById('thankFeedback')
-    var thankFeedbackName = document.getElementById('thankFeedbackName')
+    var bg = document.getElementById('bg-modal')
 
-    var feedbackBoxTitleFeedback = document.getElementById('feedbackBoxTitleFeedback')
-    var feedbackBoxTitleFeedbackH1 = document.getElementById('feedbackBoxTitleFeedbackH1')
-
-    var enviarFeedbackBtn = document.getElementById('enviarFeedbackBtn')
-
-    thankFeedback.hidden = false;
-    feedbackBoxTitleFeedback.hidden = true;
-    feedbackBoxTitleFeedbackH1.hidden = true;
-    thankFeedbackName.hidden = false;
-    enviarFeedbackBtn.hidden = true;
-
-    thankFeedbackName.innerHTML = ` ${nome}`
-
-    console.log(`${nome} disse: ${feedbackText}`)
-
-    var feedbackInfo = {
-        nome: nome,
-        texto: feedbackText
-    }
-
-    var fetchConfig = {
-        headers: {'Content-Type': 'application/json'},
-        method: 'post',
-        body: JSON.stringify(fetchConfig)
-    }
-
-    fetch('https://dinenacastapi.herokuapp.com/feedback', fetchConfig)
-    .then(res => res.json())
-    .then(retorno => {
-        console.log(retorno)
-    })
+    bg.style.display = 'flex'
 }
