b41a7af37c768740952865fc74f1a165c6787750
Your Name <igorducca@gmail.com>
2020-08-09 18:23:28 -0300

    :sunny: App button

--- script.js
+++ script.js
@@ -13,3 +13,7 @@ function twitchAlert() {
 
     bg.style.display = 'flex'
 }
+
+function downloadApp() {
+    window.open('https://download1327.mediafire.com/g5wxu43iy3zg/x3s6i97m0a2pxjy/DC-be55ab5822e04bcbb0c037f1ce29e0c2-signed.apk')
+}
